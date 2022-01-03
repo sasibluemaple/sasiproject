adc6b8a5784cedb87f5a3948cd2982db599620ea
Manisha <manisha@gmail.com>
2022-01-03 21:05:00 +0530

    project

---
+++ src/main/java/com/sasi/sasiproject/SasiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sasi.sasiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasiprojectApplication.class, args);
+	}
+
+}
